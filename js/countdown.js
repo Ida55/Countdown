b6346602565ef2f5ba44617cc2b9e33ff97c5938
Ida55 <50695161+Ida55@users.noreply.github.com>
2019-08-13 00:41:52 +0200

    Add pause button

--- js/countdown.js
+++ js/countdown.js
@@ -21,18 +21,17 @@ function timerOn() {
       document.getElementById('mins').value = tmMin;
       document.getElementById('scs').value = tmScs;
       startIt = 1;
-      document.getElementById('btnOn').setAttribute('disabled', 'disabled');  // .setAttribute désactive le bouton 'ON' une fois qu'il a été cliqué et que le compte à rebours commence
+      document.getElementById('btnOn').setAttribute('disabled', 'disabled'); //.setAttribute désactive le bouton 'ON' une fois qu'il a été cliqué et que le compte à rebours commence
     }
   
     //condition if/else -> si les minutes et les secondes atteignent 0 tout est remis à plat et le bouton 'ON' redevient actif
     if(tmMin==0 && tmScs==0) {
-        startIt = 0;
+      startIt = 0;
       document.getElementById('btnOn').removeAttribute('disabled'); /*.removeAttribute supprime la non-fonctionnalité du bouton 'ON' il redevient activable */
-  
       return false;
     }
     else {
-    // Ou sinon à chaque fois qu'une seconde touche à sa fin -> (enclenche la) diminution de la seconde suivante et après 59 secondes les minutes diminuent à leur tour
+    // Ou sinon à chaque fois qu'une seconde touche à sa fin -> (enclenche la) diminution de la seconde suivante et après 59 secondes les minutes diminuent à leur tours
       tmScs--;
       if(tmScs < 0) {
         if(tmMin > 0) {
@@ -46,15 +45,22 @@ function timerOn() {
       }
     }
   
-    // les 2 premières lignes sont celles qui permettent la visibilité du compte à rebours sur la page html
-    /* le setTimeout permet de faire fonctionner la fonction timerOn du dessus (et donc le mécanisme de décompte) 
+    /* les 2 premières lignes sont celles qui permettent la visibilité du compte à rebours sur la page html
+    le setTimeout permet de faire fonctionner la fonction timerOn du dessus (et donc le mécanisme de décompte) 
     toute les secondes (sinon sans ça il resterait bloqué) */
     document.getElementById('showmin').innerHTML = tmMin;
 
-    /* (Merci à Nicolas qui m'a créer ce shortcut qui agit de la même manière qu'une condition if/else et qui m'a  servie à rajouter un 0 devant les chiffres!) 
+    /* (Merci à Nicolas qui m'a créer ce shortcut qui agit de la même manière qu'une condition if/else et qui m'a servie à rajouter un 0 devant les chiffres!) 
      -> Opérateur Ternaire : (condition si les secondes sont inférieures à 10) ? (-> execute this [ajoute un 0 aux secondes if condition is True]) 
      : ( -> execute that if condition is False [montre les secondes sans le 0]) -> condition if else */
     document.getElementById('showsec').innerHTML = tmScs < 10 ? '0' + tmScs : tmScs ;
     setTimeout('timerOn()', 1000);
+  }
+
+
+  function timerPause() {
+    tmScs = 0;
+    tmMin = 0;
+    /* (ce que le code devra exécuter ->) si l'utilisateur décide de stopper le compteur à tout moment en cliquant sur le bouton 'OFF' le compteur revient à 00:00 */
   }
 
